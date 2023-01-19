8710535c8f8e46be5ee6a35f531c7302c48a9d4c
Ewie1 <shephvo12@gmail.com>
2023-01-19 19:59:25 +0000

    Download django allauth, apply settings to handle user registration

--- gymwebsite/urls.py
+++ gymwebsite/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from goodlife.views import get_base_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_base_page, name='get_base_page')
+    path('', get_base_page, name='get_base_page'),
+    path('accounts/', include('allauth.urls')),
 ]
